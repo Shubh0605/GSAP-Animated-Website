cc79c646c4977e33f7a4e08e5dae7e3131ecda55
Shubham Kumar <103112458+Shubh0605@users.noreply.github.com>
2025-09-02 03:08:06 +0530

    Update Project.jsx

--- K72 yt/src/pages/Project.jsx
+++ K72 yt/src/pages/Project.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Video from '../Components/Home/video'
 import ProjectCard from '../Components/Projects/ProjectCard'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
